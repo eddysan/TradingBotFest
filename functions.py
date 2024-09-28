3edc80ae2ca9b04b36e3642507499c44d12519c3
Eddy D. Sanchez <eddy.sanchez@gmail.com>
2024-09-27 22:13:39 -0400

    create grid and post to binance, done

--- functions.py
+++ functions.py
@@ -1,19 +1,19 @@
 import numpy as np
 import json
 #import math
-#from binance.client import Client
-import base64
+from binance.client import Client
+#import base64
 import requests
 #from websocket import create_connection
 #import time
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
+#from cryptography.hazmat.primitives.serialization import load_pem_private_key
 
 
-# load json with default data, this json represent the entire grid for operations
-def load_config():
+# load json config
+def load_config(json_file):
   try:
     # Load the JSON file with default config
-    with open('config.json', 'r') as file:
+    with open(json_file, 'r') as file:
       data = json.load(file)
   except Exception as e:
     print(f"Error loading configuration: {e}")
@@ -34,16 +34,16 @@ def generate(data_grid):
   new_price = 0
   sl_amount = data_grid['sl_amount']
   current_price = data_grid["entry_price"]
-  current_token = data_grid["entry_token"]
+  current_token = data_grid["entry_quantity"]
   new_distance = data_grid['grid_distance']
 
   # percentage array for every entry
   p = np.array([0.0])
 
   # cost array for every entry
-  k = np.array([[ data_grid['entry_price'] * data_grid['entry_token'] ]])
+  k = np.array([[ data_grid['entry_price'] * data_grid['entry_quantity'] ]])
 
-  data_grid['entry_order'].append({"entry" : 0, "price" : data_grid['entry_price'], "tokens" : data_grid['entry_token'], "cost" : data_grid['entry_price'] * data_grid['entry_token']})
+  data_grid['entry_order'].append({"g_entry" : 0, "g_price" : round(data_grid['entry_price'],price_decimal), "g_quantity" : round(data_grid['entry_quantity'],quantity_decimal), "g_cost" : data_grid['entry_price'] * data_grid['entry_quantity']})
 
   while lost_amount <= sl_amount:
   
@@ -68,7 +68,7 @@ def generate(data_grid):
       k = np.append(k,[[new_price * new_token]], axis=0)
 
       # update to list
-      data_grid['body_order'].append({"entry" : len(data_grid['body_order'])+1, "price" : new_price, "tokens" : new_token, "cost" : new_price * new_token})
+      data_grid['body_order'].append({"g_entry" : len(data_grid['body_order'])+1, "g_price" : round(new_price,price_decimal), "g_quantity" : round(new_token,quantity_decimal), "g_cost" : new_price * new_token})
 
       # update price and tokens to new
       current_price = new_price
@@ -80,66 +80,42 @@ def generate(data_grid):
   elif data_grid['grid_side'] == 'SHORT':
     sl_price = data_grid['entry_price'] * (1 + p[0].item())
 
-  data_grid['sl_order'].append({"entry" : len(data_grid['body_order'])+1, "price" : sl_price, "tokens" : current_token, "cost" : sl_price * current_token})
+  data_grid['sl_order'].append({"g_entry" : len(data_grid['body_order'])+1, "g_price" : round(sl_price,price_decimal), "g_quantity" : round(current_token,quantity_decimal), "g_cost" : sl_price * current_token})
 
   return data_grid
 
 
 def post_order(order_data):
+    new_order = []
   
-  # Set up authentication
-  API_KEY='9moGAdDl4pkFCWb69qU5B8fCWJ24p32lnmMMX6kHSwuPSwy9nuwkrnxoNX6msa1P'
-  PRIVATE_KEY_PATH='../binance_private_key.txt'
-
-  # Load the private key.
-  # In this example the key is expected to be stored without encryption,
-  # but we recommend using a strong password for improved security.
-  with open(PRIVATE_KEY_PATH, 'rb') as f:
-    private_key = load_pem_private_key(data=f.read(), password=None)
-
-  # setting parameters for request
-    params = {
-      'symbol':       data_grid['token_pair'].upper(),
-      'side':         'BUY' if data_grid['grid_side'] == 'LONG' else 'SELL',
-      'type':         'LIMIT',
-      'timeInForce':  'GTC',
-      'dualSidePosition': True,
-      'positionSide': data_grid['grid_side']
-    }
-  
-  # Timestamp the request
-  #timestamp = int(time.time() * 5000) # UNIX timestamp in milliseconds
-  params['timestamp'] = get_binance_server_time()
-  params['recvWindow'] = 10000
-
-  # Sign the request
-  payload = '&'.join([f'{param}={value}' for param, value in params.items()])
-  signature = base64.b64encode(private_key.sign(payload.encode('ASCII')))
-  params['signature'] = signature.decode('ASCII')
-
-  # Send the request
-  headers = {
-      'X-MBX-APIKEY': API_KEY,
-  }
-
-  for order in order_data:
-    # Set up the request parameters
-    params['quantity'] = str(order['tokens'])
-    params['price'] = str(order['price'])
-
-    response = requests.post(
-      'https://fapi.binance.com/fapi/v1/order',
-      headers=headers,
-      data=params,
-    )
-
-    # must add the result to grid
-    order_data.append(response)
-    print(response)
-    return order_data
+    # Set up authentication
+    binance_config_file = load_config('../credentials.json')  
+    api_key = binance_config_file['api_key']
+    api_secret = binance_config_file['api_secret']
+    
+    client = Client(api_key, api_secret)
+    
+    for order in order_data:
+        
+        response = client.futures_create_order(
+            symbol = data_grid['token_pair'].upper(),
+            side = 'BUY' if data_grid['grid_side'] == 'LONG' else 'SELL',
+            type = 'LIMIT',
+            timeInForce = 'GTC',
+            positionSide = data_grid['grid_side'],
+            price = str(order['g_price']),
+            quantity = str(order['g_quantity'])
+            )
+        new_order.append(order | response)
+    
+    
+        # must add the result to grid
+    return new_order
 
 
-data_grid = load_config()
+##############################################################################
+
+data_grid = load_config('config.json')
 
 # try input and apply default values
 #gridSide = input("Grid Side (long/short): ") or 'long'
@@ -150,14 +126,17 @@ data_grid = load_config()
 #entryToken = float(input("Entry Token: ") or 0)
 
 # default variables to dev
-data_grid['token_pair'] = 'ADAUSDT'
-data_grid['grid_side'] = 'LONG'
-data_grid['grid_distance'] = 2 /100
-data_grid['token_increment'] = 40 / 100
+data_grid['token_pair'] = 'ETHUSDT'
+data_grid['grid_side'] = 'SHORT'
+data_grid['grid_distance'] = 0.02
+data_grid['token_increment'] = 0.40
 data_grid['sl_amount'] = 10.00
-data_grid['entry_price'] = 0.3270
-data_grid['entry_token'] = 28
+data_grid['entry_price'] = 2750.00
+data_grid['entry_quantity'] = 0.008
 
+# get decimal places in order to round in table
+price_decimal = str(data_grid['entry_price'])[::-1].find('.')
+quantity_decimal = str(data_grid['entry_quantity'])[::-1].find('.')
 
 # You can then proceed to use the generate function
 data_grid = generate(data_grid)
@@ -168,24 +147,3 @@ print(data_grid)
 data_grid['body_order'] = post_order(data_grid['body_order'])
 print(data_grid)
 
-# ED25519 Keys
-#apiKey = "wmofsFgVdJjppz09nNoMe5JVxOpU3TM7NNq5eSfJm0MGo3PoW196CY6BtOCRN5DF"
-#privateKey = "MC4CAQAwBQYDK2VwBCIEII7CkdD8SF5EtHogmn5Ktiluc+cEsp0GakkJwDBpb8QA"
-#privateKeyPass = "<password_if_applicable>"
-
-
-#with open(privateKey, 'rb') as f:
-#    privateKey = f.read()
-
-#client = Client(api_key=apiKey, private_key=privateKey)
-
-
-# Now the instance variables are loaded from the JSON
-#print(calc.gridSide)
-#print(calc.gridDistance)
-#print(calc.entryPrice)
-
-#print(tab)
-
-
-    
--- futuresya.py
+++ futuresya.py
@@ -6,10 +6,24 @@ Created on Tue Sep 24 19:57:27 2024
 @author: eddysan
 """
 from binance.client import Client
+import json
 
-test_key = "uP1Hh54WChLGH0a81MqtNvxukonEhZWUUDyzebxtecsoiCIcy2AYHLfPYKrG8opF"
-test_secret_key = "8JVTDWtYJhWfwDrrbY4Zh0T7xSCkMr2A7dkKvPjrWNPcuwj798UAuprGU7DomoST"
-client = Client(test_key, test_secret_key)
+# load json config
+def load_config(json_file):
+  try:
+    # Load the JSON file with default config
+    with open(json_file, 'r') as file:
+      data = json.load(file)
+  except Exception as e:
+    print(f"Error loading configuration: {e}")
+  return data
+
+
+binance_credentials = load_config('../credentials.json')
+
+api_key = binance_credentials['api_key']
+api_secret = binance_credentials['api_secret']
+client = Client(api_key, api_secret)
 
 symbol = 'ETHUSDT'
 tar_profit = 0.09 #take profit when ROE hits 9%
@@ -19,17 +33,19 @@ ticker_data = client.futures_symbol_ticker(symbol = symbol)
 current_price = float(ticker_data["price"])
 cp_adder = 1 + float(tar_profit / lev)
 tp_price = round(current_price * cp_adder, 2)
-qty = 0.2
 
-client.futures_create_order(
+
+res = client.futures_create_order(
         symbol = symbol,
         side = 'BUY',
         type = 'LIMIT',
         timeInForce = 'GTC',
         price = 2570.00,
         quantity = 0.008,
+        positionSide = 'LONG'
         #isolated=True,
         #stopPrice=stop_price,
-        workingType='CONTRACT_PRICE' #or MARK PRICE
+        #workingType='CONTRACT_PRICE' #or MARK PRICE
         )
 
+print(res)
