28dea6b9a737b73053ac47e04e940bd71af00d99
Eddy D. Sanchez <eddy.sanchez@gmail.com>
2024-12-20 13:08:57 -0400

    some fixes

--- package_recoveryzone.py
+++ package_recoveryzone.py
@@ -134,10 +134,12 @@ class RecoveryZone:
             write_config_data('ops',f"{self.symbol}.json",self.data_grid)
 
         if message['o']['ot'] == 'TAKE_PROFIT_MARKET' and message['o']['cp'] == True:  # take profit and close position
-            clean_all_open_orders(self.symbol)
+            logging.info(f"{self.symbol}_{self.pos_side} - Take profit order taken")
+            clean_open_orders(self.symbol, self.pos_side)
 
         if message['o']['ot'] == 'STOP_MARKET' and message['o']['cp'] == True:  # take profit and close position
-            clean_all_open_orders(self.symbol)
+            logging.info(f"{self.symbol}_{self.pos_side} - Stop loss order taken")
+            clean_open_orders(self.symbol, self.pos_side)
 
     def update_current_position(self):
         logging.debug(f"{self.symbol} UPDATING CURRENT POSITION...")
