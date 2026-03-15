3bc7751c15e475b0c539f9586ced42d8c203a901
Eddysan <eddysan@debiancito>
2026-03-15 17:06:21 -0400

    update libraries

--- update.sh
+++ update.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# getting latest updates from github
 git fetch --all
 git reset --hard origin/main
+git pull origin main
 
+# activating virtual environment
+source .venv/bin/activate
+
+# installing dependencies
+pip install -r requirements.txt
+
+# setting up user service
+mkdir -p ~/.config/systemd/user
+cp tradingbot.service ~/.config/systemd/user/
+systemctl --user daemon-reload
+systemctl --user enable tradingbot
+
+# restarting service
+systemctl --user restart tradingbot
